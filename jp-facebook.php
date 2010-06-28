3cc923ac0aaf5169b72fe6fd24f007f7bd6a076e
jonpreecebsc <jonpreecebsc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-28 13:25:11 +0000

    Added Widgets feature
    git-svn-id: https://plugins.svn.wordpress.org/jp-facebook/trunk@258211 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jp-facebook.php
+++ jp-facebook.php
@@ -3,13 +3,12 @@
 Plugin Name: JP-Facebook
 Plugin URI: http://www.jpreece.com/tutorials/wordpress/jp-facebook/
 Description: Adds a Facebook 'Like' button to any page on your blog
-Version: 0.2
+Version: 0.3
 Author: Jonathan Preece
 Author URI: http://www.jpreece.com
 License: GPL2
 */
-?>
-<?php
+
 /*  Copyright 2010 Jonathan Preece  (email : info@jpreece.com )
 
     This program is free software; you can redistribute it and/or modify
@@ -25,37 +24,81 @@ License: GPL2
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-?>
-<?php
 
-//Acts as a handler for the Summary shortcode
+/* Add our function to the widgets_init hook. */
+add_action( 'widgets_init', 'jpfb_load_widgets' );
+
+/* Function that registers our widget. */
+function jpfb_load_widgets()
+{
+	register_widget( 'JPFB_Widget' );
+}
+
+class JPFB_Widget extends WP_Widget
+{
+	function JPFB_Widget()
+	{
+		/* Widget settings. */
+		$widget_ops = array( 'classname' => 'jpfb', 'description' => 'Adds a Facebook "Like" button to your theme.');
+
+		/* Widget control settings. */
+		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'jpfb-widget' );
+
+		/* Create the widget. */
+		$this->WP_Widget( 'jpfb-widget', 'JP-Facebook', $widget_ops, $control_ops );
+	}
+	
+	function widget( $args, $instance )
+	{
+		extract( $args );
+
+		/* User-selected settings. */
+		$title = apply_filters('widget_title', $instance['title'] );
+
+		/* Before widget (defined by themes). */
+		echo $before_widget;
+
+		/* Title of widget (before and after defined by themes). */
+		if ( $title )
+			echo $before_title . $title . $after_title;
+			
+		echo '<div style="text-align:center; width:200px; border: none"><iframe src="http://www.facebook.com/plugins/like.php?href=' . get_permalink() . '&amp;layout=button_count&amp;show_faces=true&amp;width=50&amp;action=like&amp;font=arial&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border: none; width:70px; height:21px; padding:10px"></iframe></div>';
+
+		/* After widget (defined by themes). */
+		echo $after_widget;
+	}
+	
+	function update( $new_instance, $old_instance )
+	{
+		$instance = $old_instance;
+
+		/* Strip tags (if needed) and update the widget settings. */
+		$instance['title'] = strip_tags( $new_instance['title'] );
+
+		return $instance;
+	}
+	
+	function form( $instance )
+	{
+		/* Set up some default widget settings. */
+		$defaults = array( 'title' => '' );
+		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+        
+        <p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+		</p>
+
+        <?php
+	}        
+}
+
 function facebooklike_func()
 {			
 	echo '<iframe src="http://www.facebook.com/widgets/like.php?href=' . get_permalink() . '" 
         scrolling="no" frameborder="0" style="border:none; width:450px; height:80px"></iframe>';
 }
 
-//add_action('admin_menu', 'my_plugin_menu');
-//
-//function my_plugin_menu() {
-//
-//  add_options_page('JP-Facebook Options', 'JP-Facebook', 'manage_options', 'my-unique-identifier', 'my_plugin_options');
-//
-//}
-//
-//function my_plugin_options() {
-//
-//  if (!current_user_can('manage_options'))  {
-//    wp_die( __('You do not have sufficient permissions to access this page.') );
-//  }
-//
-//  echo '<div class="wrap">';
-//  echo '<p>Here is where the form would go if I actually had options.</p>';
-//  echo '</div>';
-//
-//}
-
-//Adds shortcode [Facebook-Like]
 add_shortcode('JP-Facebook-Like', 'facebooklike_func');
 
 ?>
